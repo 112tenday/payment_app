4342bf7f169f750bb5fb482b6affd2eee8e0d871
Iwan Gunawan Tondang <iwan.gunawan@edi-indonesia.co.id>
2024-11-08 13:51:03 +0700

    add cross biar bisa deploy

--- src/main/java/api/nutech/controller/InformationController.java
+++ src/main/java/api/nutech/controller/InformationController.java
@@ -1,17 +1,30 @@
 package api.nutech.controller;
 
+import api.nutech.dto.BannerResponse;
+import api.nutech.service.BannerService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/information")
 @Tag(name = "Module Information")
+@CrossOrigin("*")
 public class InformationController {
 
-    @GetMapping("/banner")
-    public String getBanner() {
-        return "Banner data";
+    @Autowired
+    private BannerService bannerService;
+
+    @GetMapping ("/banner")
+    public ResponseEntity<List<BannerResponse>> getAllBanners()
+    {
+        List<BannerResponse> bannerResponses = bannerService.getAllBanners();
+        return ResponseEntity.ok(bannerResponses);
     }
+
 
     @GetMapping("/services")
     public String getServices() {
